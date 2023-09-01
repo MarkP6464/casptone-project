f40a6d4ef85f799da6370956e6dd75e3906da610
MarkP6464 <quangminhphamtran@gmail.com>
2023-09-01 11:23:23 +0700

    Initial project

---
+++ src/main/java/com/example/capstoneproject/controller/Welcome.java
@@ -0,0 +1,28 @@
+package com.example.capstoneproject.controller;
+
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("")
+@RestController
+public class Welcome {
+
+    @Operation(summary = "Get a book by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content) })
+    @GetMapping("")
+    public String hello(@RequestParam String name){
+        return "hello" + name;
+    }
+}
