1ec2121711be39aa68c42c32a2ccd1f4109e3138
anhbang369 <damanhbangthpt@gmail.com>
2023-09-04 09:08:37 +0700

    init mapper cv

---
+++ src/main/java/com/example/capstoneproject/MapperConfig.java
@@ -0,0 +1,20 @@
+package com.example.capstoneproject;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setSkipNullEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFullTypeMatchingRequired(true);
+        return modelMapper;
+    }
+}
