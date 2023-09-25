3313b4eec5e0c2e33b16ca91827c1bfa1478c47f
MarkP6464 <quangminhphamtran@gmail.com>
2023-09-25 17:39:23 +0700

    fix CORS problems

--- src/main/java/com/example/capstoneproject/CapstoneProjectApplication.java
+++ src/main/java/com/example/capstoneproject/CapstoneProjectApplication.java
@@ -1,8 +1,14 @@
 package com.example.capstoneproject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//@OpenAPIDefinition(
+//		servers = {
+//				@Server(url = "https://cvbuilder-api.monoinfinity.net", description = "Default Server URL")
+//		}
+//)
 @SpringBootApplication
 public class CapstoneProjectApplication {
 
