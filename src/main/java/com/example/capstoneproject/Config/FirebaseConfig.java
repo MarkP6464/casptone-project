d01cbf6be46a52611951c211a497517acf3a9e7e
MarkP6464 <quangminhphamtran@gmail.com>
2023-11-17 12:23:33 +0700

    update FileInputStream in firebaseConfig

--- src/main/java/com/example/capstoneproject/Config/FirebaseConfig.java
+++ src/main/java/com/example/capstoneproject/Config/FirebaseConfig.java
@@ -7,19 +7,23 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 @PostConstruct
 public void initialize() throws IOException {
 
-    FileInputStream serviceAccount =
-            new FileInputStream("src/main/resources/serviceAccountKey.json");
+//    FileInputStream serviceAccount =
+//            new FileInputStream("/serviceAccountKey.json");
+
+    InputStream serviceAccount = this.getClass().getResourceAsStream("/serviceAccountKey.json");
 
     FirebaseOptions options = new FirebaseOptions.Builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
