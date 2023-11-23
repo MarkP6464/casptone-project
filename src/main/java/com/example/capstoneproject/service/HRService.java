fa3f69f885806f882368032fa369946b78a271c9
MarkP6464 <quangminhphamtran@gmail.com>
2023-11-23 18:16:31 +0700

    create HR

---
+++ src/main/java/com/example/capstoneproject/service/HRService.java
@@ -0,0 +1,10 @@
+package com.example.capstoneproject.service;
+
+import com.example.capstoneproject.Dto.NoteDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface HRService {
+
+}
---
+++ src/main/java/com/example/capstoneproject/service/impl/HRServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.capstoneproject.service.impl;
+
+import com.example.capstoneproject.service.HRService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HRServiceImpl implements HRService {
+}
