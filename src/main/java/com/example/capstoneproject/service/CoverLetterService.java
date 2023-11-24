fdaa2c5e0512a85cb22e7d2fefd982a4284c0728
anhbang369 <damanhbangthpt@gmail.com>
2023-11-24 18:42:38 +0700

    choice cv when create cover letter

--- src/main/java/com/example/capstoneproject/service/CoverLetterService.java
+++ src/main/java/com/example/capstoneproject/service/CoverLetterService.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Service
 public interface CoverLetterService extends BaseService<CoverLetterDto, Integer>{
-    CoverLetterViewDto createCoverLetter(Integer userId, CoverLetterAddDto dto);
+    CoverLetterViewDto createCoverLetter(Integer userId, Integer cvId, CoverLetterAddDto dto);
     List<CoverLetterViewDto> getAllCoverLetter(Integer userId);
     boolean updateCoverLetter(Integer cvId, Integer coverLetterId, CoverLetterUpdateDto dto);
     boolean deleteCoverLetterById(Integer UsersId,Integer coverLetterId);
