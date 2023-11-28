e296c043d7525f2261d22136e24f4404df28a108
MarkP6464 <quangminhphamtran@gmail.com>
2023-11-28 23:59:38 +0700

    change author

--- src/main/java/com/example/capstoneproject/controller/CvController.java
+++ src/main/java/com/example/capstoneproject/controller/CvController.java
@@ -22,31 +22,31 @@ public class CvController {
     }
 
     @GetMapping("/{user-id}/cvs")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('read:candidate', 'read:expert')")
     public List<CvViewDto> getCvsById(@PathVariable("user-id") Integer UsersId, @RequestParam(required = false) String content) {
         return cvService.GetCvsById(UsersId, content);
     }
 
     @PostMapping("/user/{user-id}/cv/{cv-id}/duplicate")
-    @PreAuthorize("hasAuthority('create:candidate')")
+    @PreAuthorize("hasAnyAuthority('create:candidate', 'create:expert')")
     public CvDto duplicationCv(@PathVariable("user-id") Integer userId, @PathVariable("cv-id") Integer cvId) throws JsonProcessingException {
         return  cvService.duplicateCv(userId, cvId);
     }
 
     @GetMapping("/{user-id}/cv/{cv-id}")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('read:candidate', 'read:expert')")
     public CvAddNewDto getCvAndRelationsByCvId(@PathVariable("user-id") int UsersId, @PathVariable("cv-id") int cvId) throws JsonProcessingException {
         return cvService.GetCvByCvId(UsersId, cvId);
     }
 
     @PostMapping("/{user-id}/cv")
-    @PreAuthorize("hasAuthority('create:candidate')")
+    @PreAuthorize("hasAnyAuthority('create:candidate', 'create:expert')")
     public CvAddNewDto postCv(@PathVariable("user-id") int UsersId, @RequestBody CvBodyDto Dto) throws JsonProcessingException {
         return cvService.createCv(UsersId, Dto);
     }
 
     @PutMapping("/{user-id}/cv/{cv-id}/cv-body")
-    @PreAuthorize("hasAuthority('update:candidate')")
+    @PreAuthorize("hasAnyAuthority('update:candidate', 'update:expert')")
     public String updateCvBody(@PathVariable("user-id") int UsersId, @PathVariable("cv-id") int cvId, @RequestBody CvBodyDto Dto) throws JsonProcessingException {
         boolean check = cvService.updateCvBody(cvId, Dto);
         if (check) {
@@ -57,13 +57,13 @@ public class CvController {
     }
 
     @GetMapping("/cv/{cv-id}/finish-up")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('update:candidate', 'update:expert')")
     public CvAddNewDto getFinishUp(@PathVariable("cv-id") int cvId) throws JsonProcessingException {
         return cvService.finishUp(cvId);
     }
 
     @PutMapping("/{user-id}/cv/{cv-id}/summary")
-    @PreAuthorize("hasAuthority('update:candidate')")
+    @PreAuthorize("hasAnyAuthority('update:candidate', 'update:expert')")
     public String updateSummary(@PathVariable("user-id") int UsersId, @PathVariable("cv-id") int cvId, @RequestBody CvUpdateSumDto Dto) {
         boolean check = cvService.updateCvSummary(UsersId, cvId, Dto);
         if (check) {
@@ -74,26 +74,26 @@ public class CvController {
     }
 
     @PutMapping("/{user-id}/contact")
-    @PreAuthorize("hasAuthority('update:candidate')")
+    @PreAuthorize("hasAnyAuthority('update:candidate', 'update:expert')")
     public UsersViewDto updateContact(@PathVariable("user-id") int UsersId, @RequestBody UsersViewDto dto) {
         return cvService.updateCvContact(UsersId, dto);
     }
 
     @DeleteMapping("/{user-id}/cv/{cv-id}")
-    @PreAuthorize("hasAuthority('delete:candidate')")
+    @PreAuthorize("hasAnyAuthority('update:candidate', 'update:expert')")
     public String deleteCv(@PathVariable("user-id") int UsersId, @PathVariable("cv-id") int cvId) {
         cvService.deleteCvById(UsersId, cvId);
         return "Delete successful";
     }
 
     @GetMapping("{user-id}/cv/{cv-id}/evaluate")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('update:candidate', 'update:expert')")
     public List<ScoreDto> getEvaluateCV(@PathVariable("user-id") int userId, @PathVariable("cv-id") int cvId) throws JsonProcessingException {
         return cvService.getEvaluateCv(userId, cvId);
     }
 
     @PutMapping("/{user-id}/cv/{cv-id}/public")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('read:candidate', 'read:expert')")
     public String searchable(@PathVariable("user-id") int userId, @PathVariable("cv-id") int cvId) {
         boolean check = cvService.searchable(userId, cvId);
         if (check) {
@@ -104,26 +104,26 @@ public class CvController {
     }
 
     @GetMapping("/cvs/public")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('read:candidate', 'read:expert')")
     public List<CvAddNewDto> getSearchable(@RequestParam(required = false) String field) {
         return cvService.getListSearchable(field);
     }
 
     @GetMapping("/{user-id}/cvs/resume")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('read:candidate', 'read:expert')")
     public List<CvResumeDto> getListResume(@PathVariable("user-id") Integer userId) {
         return cvService.getListResume(userId);
     }
 
     @GetMapping("/{user-id}/cv/{cv-id}/experiences/role-names")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('read:candidate', 'read:expert')")
     public List<ExperienceRoleDto> getListExperienceRole(@PathVariable("user-id") Integer userId, @PathVariable("cv-id") Integer cvId) throws JsonProcessingException {
         return cvService.getListExperienceRole(userId,cvId);
     }
 
 
     @GetMapping("/{userId}/cv/{cvId}/micro")
-    @PreAuthorize("hasAuthority('read:candidate')")
+    @PreAuthorize("hasAnyAuthority('read:candidate', 'read:expert')")
     public Cv findByUserIdAndId(@PathVariable("userId") Integer userId, @PathVariable("cvId") Integer cvId) throws JsonProcessingException {
         return cvService.findByUser_IdAndId(userId, cvId);
     }
