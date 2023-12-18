43aaba14c691c8882792a015a676a21ff972b273
anhbang369 <damanhbangthpt@gmail.com>
2023-12-18 12:24:29 +0700

    change type in database

--- src/main/java/com/example/capstoneproject/entity/SectionLog.java
+++ src/main/java/com/example/capstoneproject/entity/SectionLog.java
@@ -14,6 +14,7 @@ public class SectionLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(columnDefinition = "TEXT")
     private String Bullet;
 
     private Integer count;
